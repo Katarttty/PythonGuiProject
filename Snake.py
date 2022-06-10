3b8f9b4614b788d08cb16231678aa3386757f0b8
katgr <katgreen2112@gmail.com>
2022-06-10 23:54:25 +0100

    Laptop move commit

--- Snake.py
+++ Snake.py
@@ -1,6 +1,16 @@
+import json
+
 import PySimpleGUI as sg
 from time import time
 from random import  randint
+
+
+
+filename = 'score.json'
+
+with open(filename) as openfile:
+    score_trcker = json.load(openfile)
+
 
 def convert_pos_to_Pixel(cell):
     tl = cell[0] * CELL_SIZE, cell[1] * CELL_SIZE
@@ -19,6 +29,13 @@ FILED_SIZE = 400
 CELL_NUM = 10
 CELL_SIZE = FILED_SIZE / CELL_NUM
 Score = 0
+speed = 0.5
+
+highscore_values = score_trcker.values()
+highscore_player = score_trcker
+highscore_list = list(highscore_values)
+highscore_list_player = list(highscore_player)
+highscore = highscore_list_player[0], highscore_list[0]
 
 # sanke
 snake_body = [(4,4),(3,4),(2,4)]
@@ -38,8 +55,17 @@ field = sg.Graph(
     background_color='black'
 )
 layout = [[field],
-          [sg.Text('Score:', font='Young 50'),
-           sg.Text(Score, key='_SCORE_',font='Young 50')]
+          [
+            sg.Text('Score:', font='Young 50'),
+            sg.Text(Score, key='_SCORE_',font='Young 50')
+           ],
+          [
+            sg.Text('Highest Score', key='_HIGHTSCORE_',font='Young 50')
+          ],
+          [
+            sg.Text(highscore_list_player[0], key='_HIGHTSCORE_',font='Young 20'),
+            sg.Text(': ', key='_HIGHTSCORE_',font='Young 20'),
+            sg.Text(highscore_list[0], key='_HIGHTSCORE_',font='Young 20')]
           ]
 
 window = sg.Window('Snake', layout, return_keyboard_events=True)
@@ -59,13 +85,14 @@ while True:
         direction = DIRECTIONS['down']
 
     time_since_start = time() - start_time
-    if time_since_start >= 0.5:
+    if time_since_start >= speed:
         start_time = time()
 
         # apple snake collision
         if snake_body[0] == apple_pos:
             apple_pos = polace_apple()
             apple_eaten = True
+            speed -= 0.02
             Score += 1
             window['_SCORE_'].update(Score)
 
@@ -82,6 +109,15 @@ while True:
         if not 0 <= snake_body[0][0] <= CELL_NUM - 1 or \
            not 0 <= snake_body[0][1] <= CELL_NUM - 1 or \
             snake_body[0] in snake_body[1:]:
+            print('Final Score: ', Score)
+
+
+            player_name = input('Enter Name:')
+            score_trcker[player_name] = Score
+            score_trcker = sorted(score_trcker.items(), key=lambda x: x[1], reverse=True)
+            sortdict = dict(score_trcker)
+            with open(filename, 'w') as openfile:
+                json.dump(sortdict, openfile, indent=4)
             break
 
 
